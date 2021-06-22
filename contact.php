347a0c6f482c4ef3b37ed2d9c4df86528daaaa18
MierischCoding <80060890+MierischCoding@users.noreply.github.com>
2021-06-22 12:37:59 -0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+      <!--BootStrap Links Here-->
+    <link rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="contact.css">
+
+  <!--Google Font Links Here-->
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link
+href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap"
+rel="stylesheet">
+
+  <!--CSS Links Here-->
+
+<link rel="stylesheet" href="http://www.jonathanmierisch.slccwebdev.com/CSS/contact.css">
+    <title>Contact Me</title>
+</head>
+
+<body>
+
+<header>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	<a href="http://www.jonathanmierisch.slccwebdev.com/PHP/home.php" class="navbar-brand">Home</a>
+		<img src="http://www.jonathanmierisch.slccwebdev.com/pictures/aboutme.jpg" alt="Logo" height="25" id="aboutimg">
+		<ul class="navbar-nav">
+			<button class="navbar-toggler" data-toggle="collapse"
+			data-target="#navContent"
+			aria-controls="navContent"
+			area-expanded="false"
+			aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+			<div class="collapse navbar-collapse" id="navContent">
+			<li class="navbar-item"><a href="http://www.jonathanmierisch.slccwebdev.com/PHP/about.php" title="AboutMe"class="nav-link">About Me</a></li>
+			<li class="navbar-item"><a href="http://www.jonathanmierisch.slccwebdev.com/PHP/contact.php" title="ContactMe"class="nav-link">Contact Me</a></li>
+			<li class="navbar-item"><a href="http://www.jonathanmierisch.slccwebdev.com/PHP/practice.php" title="PHP"class="nav-link">PHP Practice Page</a></li>
+		  </div>
+		</ul>
+	</nav>
+</header>
+
+<?php
+		$nameErr = $emailErr = $contBackErr = "";
+		$name = $email = $contBack = $comment = "";
+		$formErr = false;
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			
+			if (empty($_POST["name"])) {
+				$nameErr = "Name is required.";
+				$formErr = true;
+			} else {
+				$name = cleanInput($_POST["name"]);
+				//Use REGEX to accept only letters and white spaces
+				if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+					$nameErr = "Only letters and standard spaces allowed.";
+					$formErr = true;
+				}
+			}
+			
+			if (empty($_POST["email"])) {
+				$emailErr = "Email is required.";
+				$formErr = true;
+			} else {
+				$email = cleanInput($_POST["email"]);
+				// Check if e-mail address is formatted correctly
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+					$emailErr = "Please enter a valid email address.";
+					$formErr = true;
+				}
+			}
+			
+			if (empty($_POST["contact-back"])) {
+				$contBackErr = "Please let us know if we can contact you back.";
+				$formErr = true;
+			} else {
+				$contBack = cleanInput($_POST["contact-back"]);
+			}
+			
+			$comment = cleanInput($_POST["comments"]);
+		}
+
+		function cleanInput($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+	?>
+
+	<!-- Contact Form Section -->
+	<section id="contact">
+		<div class="container py-5">
+			<!-- Section Title -->
+			<div class="row justify-content-center text-center">
+				<div class="col-md-6">
+					<h2 class="display-4 font-weight-bold">Contact Me</h2>
+					<hr />
+				</div>
+			</div>
+			<!-- Contact Form Row -->
+			<div class="row justify-content-center">
+				<div class="col-6">
+				
+					<!-- Contact Form Start -->
+					<form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="POST" novalidate>
+						
+						<!-- Name Field -->
+						<div class="form-group">
+							<label for="name">Full Name:</label>
+							<span class="text-danger">*<?php echo $nameErr; ?></span>
+							<input type="text" class="form-control" id="name" placeholder="Full Name" name="name" value="<?php if(isset($name)) {echo $name;}?>"" />
+							
+						</div>
+						
+						<!-- Email Field -->
+						<div class="form-group">
+							<label for="email">Email address:</label>
+							<span class="text-danger">*<?php echo $emailErr; ?></span>
+							<input type="email" class="form-control" id="email" placeholder="name@example.com" name="email" value="<?php if(isset($email)) {echo $email;} ?>" />
+						</div>
+						
+						<!-- Radio Button Field -->
+						<div class="form-group">
+							<label class="control-label">Can we contact you back?</label>
+							<span class="text-danger">*<?php echo $contBackErr; ?></span>
+							<div class="form-check">
+								<input type="radio" class="form-check-input" name="contact-back" id="yes" value="Yes"  <?php if ((isset($contBack)) && ($contBack == "Yes")) {echo "checked";}?>/>
+								<label class="form-check-label" for="yes" >Yes</label>
+							</div>
+							<div class="form-check">
+								<input type="radio" class="form-check-input" name="contact-back" id="no" value="No" <?php if ((isset($contBack)) && ($contBack == "No")) {echo "checked";}?>/>
+								<label class="form-check-label" for="no" >No</label>
+							</div>
+						</div>
+						
+						<!-- Comments Field -->
+						<div class="form-group">
+							<label for="comments">Comments:</label>
+							<textarea id="comments" class="form-control" rows="3" name="comments"><?php if (isset($comment)) {echo $comment;} ?></textarea>
+						</div>
+
+						<!-- Required Fields Note-->
+						<div class="text-danger text-right">* Indicates required fields</div>
+						
+						<!-- Submit Button -->
+						<button class="btn btn-primary mb-2" type="submit" role="button" name="submit">Submit</button>
+					</form>
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<?php if (($_SERVER["REQUEST_METHOD"] == "POST") && (!($formErr))) { ?>
+	
+	<section id="results" style="background-color: lightsteelblue;">
+		<div class="container py-2" >
+			<div class="row ">
+				<h1>Form Entries:</h1>
+			</div>
+			<div class="row">				
+				<ul>
+					<?php
+					if ($name !== "") { echo "<li>NAME: $name </li>"; } 
+					if ($email !== "") { echo "<li>EMAIL: $email </li>"; }
+					if ($contBack !== "") { echo "<li>CONTACT BACK: $contBack </li>"; }
+					if ($comment !== "") { echo "<li>COMMENT: $comment </li>"; }
+					?>
+				</ul>		
+			</div>
+		</div>
+	</section>
+	
+	<?php } ?>
+
+<div class="word">
+    <span>T</span>
+    <span>H</span>
+    <span>A</span>
+    <span>N</span>
+    <span>K</span>
+    <BR>
+    <span>Y</span>
+    <span>O</span>
+    <span>U</span>
+    <span>!</span>
+    <p>&#128515;</p>
+</div>
+
+</body>
+
+ <!--JS Scripts Here-->
+ <script>
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
+</script>
+</div>
+</html>
